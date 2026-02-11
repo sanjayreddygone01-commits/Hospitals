e030b2463a672ca01202ae65a5e8eb5fb5b417aa
Maheshbabu7989 <193607580+Maheshbabu7989@users.noreply.github.com>
2026-02-11 16:36:18 +0530

    find all doctors

--- src/main/java/com/alpha/hospitals/controller/DoctorController.java
+++ src/main/java/com/alpha/hospitals/controller/DoctorController.java
@@ -1,10 +1,22 @@
 package com.alpha.hospitals.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alpha.hospitals.entity.Doctor;
+import com.alpha.hospitals.service.DoctoreService;
 
 @RestController
 @RequestMapping("/doctors")
 public class DoctorController {
-
+@Autowired
+	private DoctoreService doctoreService;
+@GetMapping("/findalldoctors")
+ public List<Doctor> findalldoctors(){
+	 return doctoreService.findalldoctors();
+ }
 }
--- src/main/java/com/alpha/hospitals/service/DoctoreService.java
+++ src/main/java/com/alpha/hospitals/service/DoctoreService.java
@@ -1,8 +1,21 @@
 package com.alpha.hospitals.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.alpha.hospitals.entity.Doctor;
+import com.alpha.hospitals.repository.DoctorRepo;
 
 @Service
 public class DoctoreService {
+	@Autowired
+	private DoctorRepo doctorRepo;
+
+	public List<Doctor> findalldoctors() {
+		
+		return doctorRepo.findAll();
+	}
 
 }
