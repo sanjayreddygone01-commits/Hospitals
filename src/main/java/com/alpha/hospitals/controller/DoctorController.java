fc371c7962e8976b54ff7b5331d9ead533f1a413
SaiKrishna <mskrishna0406@gmail.com>
2026-02-11 16:32:02 +0530

    insertoperation

--- src/main/java/com/alpha/hospitals/controller/DoctorController.java
+++ src/main/java/com/alpha/hospitals/controller/DoctorController.java
@@ -1,10 +1,34 @@
 package com.alpha.hospitals.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alpha.hospitals.entity.Doctor;
+import com.alpha.hospitals.service.DoctoreService;
 
 @RestController
 @RequestMapping("/doctors")
 public class DoctorController {
-
+   @Autowired
+   private DoctoreService doctoreService;
+	@PostMapping("/register")
+	public ResponseEntity<Doctor> register(@RequestBody Doctor d){
+		Doctor doc=doctoreService.register(d);
+		return new ResponseEntity<>(doc,HttpStatus.ACCEPTED);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/com/alpha/hospitals/service/DoctoreService.java
+++ src/main/java/com/alpha/hospitals/service/DoctoreService.java
@@ -1,8 +1,25 @@
 package com.alpha.hospitals.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.alpha.hospitals.entity.Doctor;
+import com.alpha.hospitals.repository.DoctorRepo;
 
 @Service
 public class DoctoreService {
-
+@Autowired
+private DoctorRepo doctorRepo;
+	public Doctor register(Doctor d) {
+		Doctor doc=new Doctor();
+		doc.setName(d.getName());
+		doc.setAge(d.getAge());
+		doc.setSalary(d.getSalary());
+		doc.setSpecialization(d.getSpecialization());
+		
+		doctorRepo.save(doc);
+		return doc;
+		
+	}
+      
 }
